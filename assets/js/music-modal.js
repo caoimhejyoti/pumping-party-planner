a6036b7eaf91012d1ee968b6966940ae16a931f6
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-20 20:42:12 +0800

    adding initital music modal js

---
+++ assets/js/music-modal.js
@@ -0,0 +1,24 @@
+// global variables
+    //API variables
+    let musicApi = 'https://api.deezer.com/version/service/id/method/?parameters';
+    let musicApiFormat = '&output=json';
+    //modal variables
+    let musicModal = document.querySelector('#music-modal');
+
+    //button variables
+    let selectMusicBtnEl = document.querySelector(".music-select");
+    console.log(selectMusicBtnEl);
+    let selectCocktailBtnEl = document.querySelector(".cocktail-select");
+
+// Function - brings up music modal dialog TODO: add next function trigger
+let musicSelectionFnc = function(){
+    musicModal.classList.remove('hidden');
+    console.log("musicSelectionFnc is reading");
+};
+
+
+
+
+
+//Select music event listener
+selectMusicBtnEl.addEventListener("click", musicSelectionFnc);
