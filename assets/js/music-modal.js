0cf0cb08139ab32f829f99d4a55000483ed61565
caoimhejyoti <caoimhejyoti@gmail.com>
2022-12-20 21:05:55 +0800

    adding genre and artist buttons

--- assets/js/music-modal.js
+++ assets/js/music-modal.js
@@ -1,14 +1,14 @@
 // global variables
-    //API variables
-    let musicApi = 'https://api.deezer.com/version/service/id/method/?parameters';
-    let musicApiFormat = '&output=json';
-    //modal variables
-    let musicModal = document.querySelector('#music-modal');
+//API variables
+let musicApi = 'https://api.deezer.com/version/service/id/method/?parameters';
+let musicApiFormat = '&output=json';
+//modal variables
+let musicModal = document.querySelector('#music-modal');
 
-    //button variables
-    let selectMusicBtnEl = document.querySelector(".music-select");
-    console.log(selectMusicBtnEl);
-    let selectCocktailBtnEl = document.querySelector(".cocktail-select");
+//button variables
+let selectMusicBtnEl = document.querySelector(".music-select");
+console.log(selectMusicBtnEl);
+let selectCocktailBtnEl = document.querySelector(".cocktail-select");
 
 // Function - brings up music modal dialog TODO: add next function trigger
 let musicSelectionFnc = function(){
