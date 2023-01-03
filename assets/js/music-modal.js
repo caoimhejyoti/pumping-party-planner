3f2d726913201a14b8c6c94e700ebb24a2833e7f
caoimhejyoti <caoimhejyoti@gmail.com>
2023-01-03 20:27:21 +0800

    adding variables + open and close modal functions

--- assets/js/music-modal.js
+++ assets/js/music-modal.js
@@ -2,22 +2,48 @@
 //API variables
 let musicApi = 'https://api.deezer.com/version/service/id/method/?parameters';
 let musicApiFormat = '&output=json';
+
+
 //modal variables
-let musicModal = document.querySelector('#music-modal');
+let musicModalEl = document.querySelector('#music-modal');
+let musicSearchResultsEl = document.querySelector('#music-search-results'); 
+let musicImageEl = document.querySelector('#music-image'); 
 
-//button variables
+//Button variables
 let selectMusicBtnEl = document.querySelector(".music-select");
-console.log(selectMusicBtnEl);
+// console.log(selectMusicBtnEl); //used for debugging
+let musicSearchBtnEl = document.querySelector("#music-search");
+// console.log(musicSearchBtnEl); //used for debugging
+let musicRandomBtnEl = document.querySelector("#music-randomize");
+// console.log(musicRansdomBtnEl); //used for debugging
+let closeMusicBtnEl = document.querySelector("#close-music-modal"); 
+// console.log(closeMusicBtnEl); //used for debugging
+var musicRadioEl = document.querySelector("#music-radio-buttons");
+// console.log(musicRadioEl); //used for debugging
 
-// Function - brings up music modal dialog TODO: add next function trigger
+// WORKING! DESCRIPTION: Function - brings up music modal dialog TODO: add next function trigger
 let musicSelectionFnc = function(){
-    musicModal.classList.remove('hidden');
-    console.log("musicSelectionFnc is reading");
+    musicModalEl.classList.remove('hidden');
+    console.log("musicSelectionFnc is reading"); //used for debugging
+};
+
+//WORKING! DESCRIPTION: function to close music modal 
+let closeMusicModalFnc = function () { 
+    musicModalEl.classList.add('hidden');
+    console.log("closeMusicModalFnc is reading"); //used for debugging
 };
 
 
 
 
 
-//Select music event listener
+
+
+//Event listeners
 selectMusicBtnEl.addEventListener("click", musicSelectionFnc);
+closeMusicBtnEl.addEventListener("click", closeMusicModalFnc);
+
+
+
+
+
